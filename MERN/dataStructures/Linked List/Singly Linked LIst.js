0ed80905ab9f301fc32ac657b5db80c0986a3b91
nahyan0077 <nahyanmp4@gmail.com>
2024-10-06 15:22:55 +0530

    singly linked list

--- MERN/dataStructures/Linked List/Singly Linked LIst.js
+++ MERN/dataStructures/Linked List/Singly Linked LIst.js
@@ -50,6 +50,60 @@ class LinkedList {
             console.log(str.slice(0,str.length - 4));
         }
     }
+    removeMid(){
+        if (this.isEmpty()) {
+            return
+        }else{
+            let slow = this.head
+            let fast = this.head
+            let prev = null
+            while (fast && fast.next) {
+                prev = slow
+                slow = slow.next
+                fast = fast.next.next
+            }
+            prev.next = slow.next
+            this.size--
+        }
+    }
+    reverse(){
+        let curr = this.head
+        let prev = null
+        while (curr) {
+            let temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        }
+        this.head = prev
+    }
+    checkPalindrom(){
+        let slow = this.head
+        let fast = this.head
+        while (fast && fast.next) {
+            slow = slow.next
+            fast = fast.next.next
+        }
+        let prev = null
+        while (slow) {
+            let temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+        }
+        let p1 = prev
+        let p2 = this.head
+        while (p2.next) {
+            if(p1.value != p2.value){
+                console.log(false);
+                return
+            }
+            p1 = p1.next
+            p2 = p2.next
+        }
+        console.log(true);
+        return
+    }
 }
 
 
@@ -59,7 +113,20 @@ li.append(1)
 li.append(2)
 li.append(3)
 li.append(4)
+li.append(5)
+
+li.prepend(1)
+li.prepend(2)
+li.prepend(3)
+li.prepend(4)
+li.prepend(5)
 
 
+
+// li.removeMid()
+
+// li.reverse()
+
+li.checkPalindrom()
 
 li.print()
